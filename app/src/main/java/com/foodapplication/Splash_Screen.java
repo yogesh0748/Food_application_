bb082c32274e9c40246fe683c7fbbd0b87203870
yogesh saini <ys8470saini@gmail.com>
2025-04-05 15:10:57 +0530

    login page done

---
+++ app/src/main/java/com/foodapplication/Splash_Screen.java
@@ -0,0 +1,33 @@
+package com.foodapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Splash_Screen extends AppCompatActivity {
+
+    private static final int Splash_Delay = 3000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash_screen);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        new Handler().postDelayed(()->{
+            Intent intent = new Intent(Splash_Screen.this, StartActivity.class);
+            startActivity(intent);
+            finish();
+        },Splash_Delay);
+    }
+}
