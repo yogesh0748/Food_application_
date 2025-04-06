f2784c382a42f3a29fd302f6d815bb8fcf53ab83
yogesh saini <ys8470saini@gmail.com>
2025-04-06 12:50:30 +0530

    signin activity layout complete

--- app/src/main/java/com/foodapplication/LoginActivity.java
+++ app/src/main/java/com/foodapplication/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.foodapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +22,15 @@ public class LoginActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        TextView tvDontHaveAccount = findViewById(R.id.donthaveaccount);
+        tvDontHaveAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, SignActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
