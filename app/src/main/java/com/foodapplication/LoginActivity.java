316f2a43f348193d74dd3796ef7a012c85bddce2
yogesh saini <ys8470saini@gmail.com>
2025-04-06 13:07:37 +0530

    Revert "signin activity layout complete"
    This reverts commit f2784c382a42f3a29fd302f6d815bb8fcf53ab83.

--- app/src/main/java/com/foodapplication/LoginActivity.java
+++ app/src/main/java/com/foodapplication/LoginActivity.java
@@ -1,9 +1,6 @@
 package com.foodapplication;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,15 +19,6 @@ public class LoginActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-
-        TextView tvDontHaveAccount = findViewById(R.id.donthaveaccount);
-        tvDontHaveAccount.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, SignActivity.class);
-                startActivity(intent);
-            }
         });
     }
 }
